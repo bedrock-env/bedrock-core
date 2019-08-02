c375b82d83d6367ba46f4f20961d374dc6d7512e
Ben Mills <ben@unfiniti.com>
2019-08-01 21:35:45 -0600

    Enable history search

--- core/config.zsh
+++ core/config.zsh
@@ -37,3 +37,10 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
