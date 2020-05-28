3ac3d082c1e2c69347b671c0f3ee83e9d1642728
Ben Mills <bmills@adobe.com>
2020-05-28 09:39:35 -0600

    Gracefully handle no bundle to load

--- bedrock.zsh
+++ bedrock.zsh
@@ -4,15 +4,24 @@ export BEDROCK_DIR=$(dirname "$0")
 
 TERM=screen-256color
 
-while read -r line || [[ $line ]]; do
-  extensions+=("$line")
-done < $BEDROCK_DIR/bundle/load
+if [ -f $BEDROCK_DIR/bundle/load ]; then
+  while read -r line || [[ $line ]]; do
+    extensions+=("$line")
+  done < $BEDROCK_DIR/bundle/load
+else
+  extensions=()
+fi
 
 setopt NULL_GLOB
+
+extension_fpaths=(${^extensions}/**/functions $fpath)
+fpath=($extension_fpaths $fpath)
+
 filesToLoad+=($BEDROCK_DIR/core/*.zsh)
 filesToLoad+=(${^extensions}/**/{path.zsh,init.zsh,completion.zsh})
 files=(${^extensions}/**/*.zsh)
 filesToLoad+=(${${${${files:#*/path.zsh}:#*/init.zsh}:#*/install.zsh}#*/update.zsh})
+
 unsetopt NULL_GLOB
 
 for file in $filesToLoad
